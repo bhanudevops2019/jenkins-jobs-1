6fc681e9d86bb7c1b7b89c40c0ffe99492623e01
Raghu K <raghuk.vit@gmail.com>
2019-02-12 06:34:11 +0530

    --- Auto-Git Commit ---

--- pipeline-jobs/multi-repos.groovy
+++ pipeline-jobs/multi-repos.groovy
@@ -1,11 +1,16 @@
 node('SLAVE') {
     stage('One') {
+      dir('CODE') {
         git 'https://github.com/citb34/studentapp-ui.git'
         sh 'ls'
+      }
     }
+
     stage('Two') {
+      dir('TERRAFORM') {
         git 'https://github.com/citb34/Terraform.git'
         sh 'ls'
+      }
     }
     stage('Three') {
         sh 'ls'
