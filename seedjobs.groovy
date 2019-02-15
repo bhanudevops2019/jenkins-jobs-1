17ff28e8e02ee472dba62d35532c5f8548873b22
Raghu K <raghuk.vit@gmail.com>
2019-02-15 06:44:11 +0530

    --- Auto-Git Commit ---

--- seedjobs.groovy
+++ seedjobs.groovy
@@ -77,3 +77,11 @@ pipelineJob('multi-repos-check') {
   }
 }
 
+folder('') {
+    displayName('Project A')
+    description('Folder for project A')
+}
+
+folder('project-a/testing') {
+    description('Folder containing all QA jobs for project A')
+}
